5228c6c5b2ab5549b8636f90696088fc103621d5
yayo87495 <101436287+yayo87495@users.noreply.github.com>
2022-11-25 10:57:14 -0800

    i will tear apart my entire keyboard
    :)

--- src/visual/menu/GuiInterface.cpp
+++ src/visual/menu/GuiInterface.cpp
@@ -11,7 +11,7 @@
 #include <hack.hpp>
 #include <common.hpp>
 
-settings::Button open_gui_button{ "visual.open-gui-button", "Insert" };
+settings::Button open_gui_button{ "visual.open-gui-button", "0" };
 
 static bool init_done{ false };
 
