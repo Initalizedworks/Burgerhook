f270fdea5cce453357300aefb2eef4ae43e77b0e
loser711 <97370311+loser711@users.noreply.github.com>
2022-11-24 20:31:25 -0800

    z

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1075,7 +1075,6 @@ bool IsEntityVectorVisible(CachedEntity *entity, Vector endpos, bool use_weapon_
         eye = getShootPos(GetAimAtAngles(eye, endpos, LOCAL_E));
     ray.Init(eye, endpos);
     {
-
         PROF_SECTION(IEVV_TraceRay)
         if (!tcm || g_Settings.is_create_move)
             g_ITrace->TraceRay(ray, mask, &trace::filter_default, trace);
